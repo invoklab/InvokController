37bb07a1cd86567be90e55b4ee20a7948d2f24d8
Thoby Noorhalim <thoby.noorhalim@outlook.com>
2021-10-13 16:10:32 +0800

    First commit

---
+++ examples/SerialMonitor/SerialMonitorDemo/SerialMonitorDemo.ino
@@ -0,0 +1,51 @@
+/*
+  Demo sketch for Controller app serial monitor
+  Written by Thoby L. Noorhalim
+  13 October 2021
+*/
+
+#include <InvokController.h>
+
+// Timer
+double elapsedTime = 0;
+double capturedTime = 0;
+
+int variable = 0;
+
+// Controller Object Instantiation
+Controller myController = Controller("websocket");
+
+// Replace SSID and PASSWORD with your preffered WiFi Credentials
+const char* SSID = "SSID";
+const char* PASSWORD = "PASSWORD";
+
+void setup() {
+  Serial.begin(115200);
+
+  // Controller Setup
+  myController.setSSID(SSID);
+  myController.setSSIDPassword(PASSWORD);
+  myController.begin();
+  myController.printIP();
+}
+
+void loop() {
+
+  // Print data every 3 second
+  elapsedTime = millis() - capturedTime;
+  if (elapsedTime > 3000 && myController.isConnected()){
+    myController.print("Hello " + to_string(variable));
+    variable++;
+    elapsedTime = 0;
+    capturedTime = millis();
+  }
+
+  if(!myController.getIncomingCommand().empty()){
+    Serial.printf("Command from client %s\n", myController.getIncomingCommand().c_str());
+    // Flush incoming command
+    myController.setIncomingCommand("");
+  }
+
+  // Controller Loop
+  myController.loop();
+}
